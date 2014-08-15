fc14b6bffe76a4bbfed2a5e13ed2d0222b28ecba
Marcel Pol <marcel@timelord.nl>
2014-08-15 13:46:10 +0200

    2.3

--- cpanel-wordpress/usr/local/cpanel/base/frontend/x3/wordpress/wordpress.php
+++ cpanel-wordpress/usr/local/cpanel/base/frontend/x3/wordpress/wordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * WordPress auto installer
- * Version 2.2
+ * Version 2.3
  * Documentation: http://timelord.nl/wordpress/cpanel-plugin-installer-for-wordpress?lang=en
 */
 
@@ -47,9 +47,56 @@ function wp_form($user_domain, $user_cpuser) {
 				<p>Select WordPress Language: 
 					<select name="wp_lang">
 						<option value="en" selected="selected">English</option>
+						<option value="sq">Albanian</option>
+						<option value="ar">Arabian</option>
+						<option value="az">Azerbaijani</option>
+						<option value="eu">Basque</option>
+						<option value="be">Bengali</option>
+						<option value="bs">Bosnian</option>
+						<option value="bg">Bulgarian</option>
+						<option value="ca">Catalan</option>
+						<option value="cn">Chinese (China)</option>
+						<option value="tw">Chinese (Taiwan)</option>
+						<option value="hr">Croatian</option>
 						<option value="nl">Dutch</option>
+						<option value="en-au">English (Australia)</option>
+						<option value="en-ca">English (Canada)</option>
+						<option value="en-gb">English (UK)</option>
+						<option value="et">Estonian</option>
+						<option value="fi">Finnish</option>
+						<option value="fr">French</option>
+						<option value="gl">Galician</option>
 						<option value="de">German</option>
+						<option value="el">Greek</option>
+						<option value="he">Hebrew</option>
+						<option value="hu">Hungarian</option>
+						<option value="is">Icelandic</option>
+						<option value="id">Indonesian</option>
+						<option value="it">Italian</option>
+						<option value="ja">Japanese</option>
+						<option value="ko">Korean</option>
+						<option value="mk">Macedonian</option>
+						<option value="nb">Norwegian (Bokmål)</option>
+						<option value="nn">Norwegian (Nynorsk)</option>
+						<option value="fa">Persian</option>
+						<option value="pl">Polish</option>
+						<option value="br">Portuguese (Brazil)</option>
+						<option value="pt">Portuguese (Portugal)</option>
+						<option value="ru">Russian</option>
+						<option value="sr">Serbian</option>
+						<option value="sk">Slovak</option>
+						<option value="sl">Slovenian</option>
+						<option value="cl">Spanish (Chile)</option>
+						<option value="es-mx">Spanish (Mexico)</option>
+						<option value="es">Spanish (Spain)</option>
+						<option value="sv">Swedish</option>
+						<option value="tl">Tagalog</option>
+						<option value="ta-lk">Tamil (Sri Lanka)</option>
 						<option value="th">Thai</option>
+						<option value="tr">Turkish</option>
+						<option value="uk">Ukrainian</option>
+						<option value="vi">Vietnamese</option>
+						<option value="cy">Welsh</option>
 					</select> 
 				</p>
 			</div>
@@ -76,14 +123,61 @@ function set_url($wp_lang) {
 	 
 	/* Locations */
 	$lang_page = Array();
+	$lang_page['ar'] = 'http://ar.wordpress.org/';
+	$lang_page['az'] = 'http://az.wordpress.org/';
+	$lang_page['be'] = 'http://be.wordpress.org/';
+	$lang_page['bg'] = 'http://bg.wordpress.org/';
+	$lang_page['br'] = 'http://br.wordpress.org/';
+	$lang_page['bs'] = 'http://bs.wordpress.org/';
+	$lang_page['ca'] = 'http://ca.wordpress.org/';
+	$lang_page['cl'] = 'http://cl.wordpress.org/';
+	$lang_page['cn'] = 'http://cn.wordpress.org/';
+	$lang_page['cy'] = 'http://cy.wordpress.org/';
 	$lang_page['de'] = 'http://de.wordpress.org/';
+	$lang_page['el'] = 'http://el.wordpress.org/';
 	$lang_page['en'] = 'http://wordpress.org/';
+	$lang_page['en-au'] = 'http://en-au.wordpress.org/';
+	$lang_page['en-ca'] = 'http://en-ca.wordpress.org/';
+	$lang_page['en-gb'] = 'http://en-gb.wordpress.org/';
+	$lang_page['es'] = 'http://es.wordpress.org/';
+	$lang_page['es-mx'] = 'http://es-mx.wordpress.org/';
+	$lang_page['et'] = 'http://et.wordpress.org/';
+	$lang_page['eu'] = 'http://eu.wordpress.org/';
+	$lang_page['fa'] = 'http://fa.wordpress.org/';
+	$lang_page['fi'] = 'http://fi.wordpress.org/';
+	$lang_page['fr'] = 'http://fr.wordpress.org/';
+	$lang_page['gl'] = 'http://gl.wordpress.org/';
+	$lang_page['he'] = 'http://he.wordpress.org/';
+	$lang_page['hr'] = 'http://hr.wordpress.org/';
+	$lang_page['hu'] = 'http://hu.wordpress.org/';
+	$lang_page['id'] = 'http://id.wordpress.org/';
+	$lang_page['is'] = 'http://is.wordpress.org/';
+	$lang_page['it'] = 'http://it.wordpress.org/';
+	$lang_page['ja'] = 'http://ja.wordpress.org/';
+	$lang_page['ko'] = 'http://ko.wordpress.org/';
+	$lang_page['mk'] = 'http://mk.wordpress.org/';
+	$lang_page['nb'] = 'http://nb.wordpress.org/';
 	$lang_page['nl'] = 'http://nl.wordpress.org/';
+	$lang_page['nn'] = 'http://nn.wordpress.org/';
+	$lang_page['pl'] = 'http://pl.wordpress.org/';
+	$lang_page['pt'] = 'http://pt.wordpress.org/';
+	$lang_page['ru'] = 'http://ru.wordpress.org/';
+	$lang_page['sk'] = 'http://sk.wordpress.org/';
+	$lang_page['sl'] = 'http://sl.wordpress.org/';
+	$lang_page['sq'] = 'http://sq.wordpress.org/';
+	$lang_page['sr'] = 'http://sr.wordpress.org/';
+	$lang_page['sv'] = 'http://sv.wordpress.org/';
+	$lang_page['ta-lk'] = 'http://ta-lk.wordpress.org/';
 	$lang_page['th'] = 'http://th.wordpress.org/';
+	$lang_page['tl'] = 'http://tl.wordpress.org/';
+	$lang_page['tr'] = 'http://tr.wordpress.org/';
+	$lang_page['tw'] = 'http://tw.wordpress.org/';
+	$lang_page['uk'] = 'http://uk.wordpress.org/';
+	$lang_page['vi'] = 'http://vi.wordpress.org/';
 
 	if ( $wp_lang == 'en' || $wp_lang == '' ) {
-	    // English version
-        $zip_url = 'http://wordpress.org/latest.zip';
+		// English version
+		$zip_url = 'http://wordpress.org/latest.zip';
 		return $zip_url;
     } else {
 		// download the webpage
@@ -185,7 +279,7 @@ function wp_install($wp_domain, $wp_user, $wp_path, $zip_url) {
 	}
 	.message {
 		 border: 1px solid #2266AA;
-		 margin: 0;
+		 margin: 20px 0 0;
 		 padding: 2%;
 		 width: 96.5%;
 	}
